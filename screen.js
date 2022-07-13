087d8d3a5611d80ceab8bee7d21a8a85e6e2d0e8
Favour Onyeaghala <onyeaghalafavourr@gmail.com>
2022-07-13 13:35:25 +0100

    hfhj

--- screen.js
+++ screen.js
@@ -1,35 +1,8 @@
- const btn = document.querySelector('button');
-btn.addEventListener('click', async ()=>{
+const menuIcon = document.querySelector(".menu-icon");
+const menuBar = document.querySelector(".menu-bar");
 
-    let stream = await navigator.mediaDevices.getDisplayMedia({
-        video:true
-    })
 
-    const mime = MediaRecorder.isTypeSupported("video/webm; codecs=vp9")
-    ? "video/webm; codecs=vp9"
-    : "video/webm"
-    let mediaRecorder = new MediaRecorder(stream,{
-        mimeType : mime
-    })
-    let chunks = []
-    mediaRecorder.addEventListener('dataavailable', (e)=>{
-      chunks.push(e.data)
-    //   console.log(e.data)
-    })
 
-    mediaRecorder.addEventListener('stop',()=>{
-        let blob = new Blob(chunks,{
-            type: chunks[0].type
-        })
-
-        let video = document.querySelector("video")
-        video.src = URL.createObjectURL(blob)
-
-        let a = document.createElement('a')
-        a.href = URL.createObjectURL(blob)
-        a.download = "video.mp4"
-        a.click()
-    })
-
-    mediaRecorder.start()
-})
+function revealMenuBar(){
+    menuBar.classList.toggle("revealMenuBar")
+}

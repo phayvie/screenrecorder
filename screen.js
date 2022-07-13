92a43d311b38df186afc4182f08ea4480cc81a01
Favour Onyeaghala <onyeaghalafavourr@gmail.com>
2022-07-13 21:17:44 +0100

    khg

--- screen.js
+++ screen.js
@@ -1,8 +1,9 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menuBar = document.querySelector(".menu-bar");
-
+const menu = document.querySelector("img");
 
 
 function revealMenuBar(){
     menuBar.classList.toggle("revealMenuBar")
+    menu.classList.toggle('rotate')
 }

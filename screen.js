eb2af452b3f8fd1e41369b30e40a5ab8e83800bc
Favour Onyeaghala <onyeaghalafavourr@gmail.com>
2022-07-16 07:23:11 +0100

    THTJ

--- screen.js
+++ screen.js
@@ -1,9 +1,34 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menuBar = document.querySelector(".menu-bar");
 const menu = document.querySelector("img");
-
+const box = document.querySelectorAll(".box")
 
 function revealMenuBar(){
-    menuBar.classList.toggle("revealMenuBar")
-    menu.classList.toggle('rotate')
+    menuBar.classList.toggle("revealMenuBar");
+    menu.classList.toggle('rotate');
 }
+ 
+window.addEventListener('load', ()=>{
+    box.forEach(boxes => {
+        windowTop=window.innerHeight;
+        var boxTop = boxes.getBoundingClientRect().top;
+        if(boxTop<windowTop){
+            boxes.classList.add('show')}
+    })
+})
+
+window.addEventListener('scroll',()=>{
+ box.forEach(boxes => {
+    windowTop=window.innerHeight-60;
+    var boxTop = boxes.getBoundingClientRect().top;
+    if(boxTop<windowTop){
+        boxes.classList.add('show')
+    }else{
+        boxes.classList.remove('show')
+    }
+
+ });
+
+})
+
+
